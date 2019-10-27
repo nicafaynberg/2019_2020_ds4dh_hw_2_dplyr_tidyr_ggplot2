ad3a321600b3514ab9a96bc645353f453ba374a2
George Moroz <agricolamz@gmail.com>
2019-10-27 04:42:44 +0300

    correct 2.1 task

--- task_2.1.R
+++ task_2.1.R
@@ -1,8 +1,8 @@
 ## task 2.1 dog names
 
-library(tidyverse)
+library("tidyverse")
 
-dog_names <- ...("https://raw.githubusercontent.com/r-classes/2019_2020_ds4dh_hw1_github/master/data/dog_names.csv")
+dog_names <- ...("https://raw.githubusercontent.com/r-classes/2019_2020_ds4dh_hw_2_dplyr_tidyr_ggplot2/master/data/dog_names.csv")
 
 dog_names %>% 
   ...
