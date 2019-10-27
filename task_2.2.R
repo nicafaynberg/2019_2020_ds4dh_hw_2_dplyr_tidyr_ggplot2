d7739e54e1d0d26e50a119af02cb4e6c13b08077
George Moroz <agricolamz@gmail.com>
2019-10-27 05:33:53 +0300

    add 2.2 task

---
+++ task_2.2.R
@@ -0,0 +1,8 @@
+## task 2.2 dog names
+
+library("tidyverse")
+
+dog_names <- ...("https://raw.githubusercontent.com/r-classes/2019_2020_ds4dh_hw_2_dplyr_tidyr_ggplot2/master/data/dog_names.csv")
+
+dog_names %>% 
+  ...
