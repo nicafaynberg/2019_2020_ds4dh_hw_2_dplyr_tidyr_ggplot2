6f80086c9c1cac9e040df7afaa254bff608d3e49
George Moroz <agricolamz@gmail.com>
2019-10-27 04:32:10 +0300

    add 1. task

---
+++ task_2.1.R
@@ -0,0 +1,11 @@
+## task 2.1 dog names
+
+library(tidyverse)
+
+dog_names <- ...("https://raw.githubusercontent.com/r-classes/2019_2020_ds4dh_hw1_github/master/data/dog_names.csv")
+
+dog_names %>% 
+  ...
+  ggplot(...) +
+  ... +
+  scale_fill_brewer(palette="Dark2")
