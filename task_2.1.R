f814f072fbfec3b5053e3e8ef3925f9258a18af2
Ника Файнберг <berenis0102@gmail.com>
2019-11-14 01:00:46 +0300

    Первое задание

--- task_2.1.R
+++ task_2.1.R
@@ -1,11 +1,17 @@
 ## task 2.1 dog names
-
-library("tidyverse")
-
-dog_names <- ...("https://raw.githubusercontent.com/r-classes/2019_2020_ds4dh_hw_2_dplyr_tidyr_ggplot2/master/data/dog_names.csv")
-
+library(tidyverse)
+library(plotly)
+dog_names <- read_csv("https://raw.githubusercontent.com/r-classes/2019_2020_ds4dh_hw_2_dplyr_tidyr_ggplot2/master/data/dog_names.csv")
 dog_names %>% 
-  ...
-  ggplot(...) +
-  ... +
+  select(name, sex) %>% 
+  count(name, sex) %>% 
+  filter(sex!="Unknown") %>%
+  arrange(desc(n)) %>% 
+  group_by(sex) %>% 
+  slice(1:20) %>% 
+  ggplot(aes(fct_reorder(name, n), n, label = (name), fill = sex)) +
+  facet_wrap(~sex, scales = "free") +
+  geom_col() +
+  labs(x = "", y = "count") +
+  coord_flip() +
   scale_fill_brewer(palette="Dark2")
